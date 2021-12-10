405417cb0008b0751d429cd9087272c93fc27ea8
Ben Mathew <benmathew@Bens-MacBook-Pro.local>
2021-12-10 16:32:54 -0600

    dropped columns

--- FinalProject.R
+++ FinalProject.R
@@ -13,8 +13,17 @@ cat("\014")
 library(tidyverse)
 
 # Before doing anything, load your data and select the sample we will use
-setwd("/Users/benmathew/Desktop/Personal/UT_CSB/S3/STA235H")
-d_total <- read.csv("TravisCountyData.csv") #Load your HMDA 2020 Travis county data here (downloaded)
+setwd("/Users/benmathew/Desktop/Personal/UT_CSB/S3/STA235H/STA235FinalProject")
+d_total <- read.csv("TravisCountyData.csv")  %>% 
+  select(-c(activity_year, lei, derived_msa.md, state_code, county_code,
+            census_tract, prepayment_penalty_term, intro_rate_period,
+            total_points_and_fees, multifamily_affordable_units, 
+            applicant_ethnicity.2, applicant_ethnicity.3,
+            applicant_ethnicity.4, applicant_ethnicity.5,
+            co.applicant_ethnicity.2, co.applicant_ethnicity.3,
+            co.applicant_ethnicity.4, co.applicant_ethnicity.5, 
+            applicant_race.2, applicant_race.3, applicant_race.4,
+            applicant_race.5))
 
 # These are the row numbers you will need (everyone will use the same observations)
 rows <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Assignments/Project/data/row_sample.csv") %>%
@@ -23,3 +32,7 @@ rows <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/examp
 d <- d_total %>% slice(rows)
 
 # Now clean your data and conduct your analysis with the d dataset.
+for(i in 1:ncol(d_total)){
+  d_total[is.na(d_total[,i]), i] <- mean(d_total[,i], na.rm = TRUE)
+}
+warnings()
